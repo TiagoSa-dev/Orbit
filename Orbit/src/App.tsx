6c88f07206900d6be17db4cf2b28b5caa1b42dd0
TiagoSa-dev <tiago.sa.3.a@gmail.com>
2026-07-21 22:26:28 +0100

    Simple acceptable UI

--- Orbit/src/App.tsx
+++ Orbit/src/App.tsx
@@ -4,20 +4,32 @@ import './App.css'
 import orbit from "./assets/orbit_logo_var_neon_noBack.png";
 function App() {
   return (
-   
-      <div className="bg-[#09090B]">
+    //The overflow hidden class is used to prevent the page from scrolling when the content overflows the viewport. 
+    // So that we have a clean black background without scrollbars.
+    <div className="bg-black h-screen overflow-hidden">
+      <h1 className="text-orange-500 text-4xl font-bold mt-4">
+        Orbit
+      </h1>
+      <div className="flex justify-center mt-4">
         <img src={orbit} alt="Orbit"
-          className="w-32 h-32 mx-auto top-left-10 "
+          className="w-32 h-32 "
         />
-
-        <h1 className="text-5xl font-bold text-orange-500">
-          Orbit
-        </h1>
-        <div className="w-32 h-32 mx-auto right-right-10">
-          <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Pink to Orange</button>
-        </div>
       </div>
-  
+
+      <form>
+        <div className="flex justify-end items-end gap-6 mt-40">
+          <input className="border rounded px-3 py-2 text-gray-700 w-70"
+           type="text" 
+           placeholder="Your text" 
+           />
+          
+          <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 rounded px-4 py-2 text-sm font-medium">
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+
   );
 }
 
